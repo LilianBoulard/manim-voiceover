d67a881e2d509a9514f6f43b94e8be6f0755d2ca
prism0x <mathblocksio@gmail.com>
2022-06-07 01:21:31 +0200

    Improved voiceover-demo, uploaded video to YouTube, added more sections to the README

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # manim-speech
 
-This is a [Manim](https://manim.community) plugin for generating voiceovers for your Manim animations.
+`manim-speech` is a [Manim](https://manim.community) plugin that helps you add voiceovers to your Manim videos directly in your code, without using a video editor. It allows you to do two very important things:
+
+1. **Develop your animation with an auto-generated AI voice** without having to re-record and re-sync the audio every time you make a change to your screenplay.
+2. When you are ready to release the final cut, **record the actual voiceover with a microphone, and replace the AI-generated voice with your recording** by changing a few lines of code.
+
+In other words, it is possible to produce entire videos without ever having to use an external video editor. Watch the YouTube video for a brief tour of the functionality offered by `manim-speech`:
+
+[![Manim-speech YouTube introduction](http://img.youtube.com/vi/3S-8fsuxHVI/0.jpg)](http://www.youtube.com/watch?v=3S-8fsuxHVI "How to add voiceovers to your Manim videos easily with manim-speech")
 
 It currently supports [Azure Text to Speech](https://azure.microsoft.com/en-us/services/cognitive-services/text-to-speech/), [gTTS](https://github.com/pndurette/gTTS/) and [pyttsx3](https://github.com/nateshmbhat/pyttsx3).
 
@@ -45,17 +52,58 @@ On Debian based distros:
 
 or install [from source](https://sourceforge.net/projects/sox/files/sox/).
 
-## Examples
+## Basic Usage
 
-See the [examples directory](./examples).
+To use `manim-speech`, you simply import the `VoiceoverScene` class from the plugin
+
+```py
+from manim_speech import VoiceoverScene
+```
+
+You make sure your Scene classes inherit from `VoiceoverScene`:
+
+```py
+class MyAwesomeScene(VoiceoverScene):
+    def construct(self):
+        ...
+```
+
+`manim-speech` offers multiple text-to-speech engines, some proprietary and some free. A good one to start with is gTTS, which uses Google Translate's proprietary API. We found out that this is the best beginner's solution in terms of cross-platform compatibility.
+
+```py
+from manim_speech import VoiceoverScene
+from manim_speech.interfaces import GTTSSpeechSynthesizer
+
+class MyAwesomeScene(VoiceoverScene):
+    def construct(self):
+        self.set_speech_synthesizer(GTTSSpeechSynthesizer())
+```
+
+The logic for adding a voiceover is pretty simple. Wrap the animation inside a `with` block that calls `self.voiceover()`:
+
+```py
+with self.voiceover(text="This circle is drawn as I speak.") as tracker:
+    ... # animate whatever needs to be animated here
+```
+
+Manim will animate whatever is inside that with block. If the voiceover hasn't finished by the end of the animation, Manim simply waits until it does. You can further use the `tracker` object for getting the total or remaining duration of the voiceover programmatically, which gives you finer control over the video:
+
+```py
+with self.voiceover(text="This circle is drawn as I speak.") as tracker:
+    self.play(Create(circle), run_time=tracker.duration)
+```
+
+Using with-blocks results in clean code, allows you to chain sentences back to back and also serve as a documentation of sorts, as the video is neatly compartmentalized according to whatever lines are spoken during the animations.
+
+See the [examples directory](./examples) for more examples. We recommend starting with the [gTTS example](https://github.com/MathBlocks/manim-speech/blob/main/examples/gtts-example.py).
 
 ## Configuring Azure
 
-The highest quality text-to-speech available to the public is currently offered by Microsoft Azure.
+The highest quality text-to-speech services available to the public is currently offered by Microsoft Azure. To use it, you need to create an Azure account.
 
-Create a file called `.env` in the same directory where you call Manim with your authentication information.
+Then, you need to find out your subscription key and service region. Check out [Azure docs](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/) for more details.
 
-For Azure, you need to specify your subscription key and service region. Check out [Azure docs](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/) for more details.
+Create a file called `.env` that contains your authentication information in the same directory where you call Manim.
 
 ```sh
 AZURE_SUBSCRIPTION_KEY="..."
--- examples/voiceover-demo.py
+++ examples/voiceover-demo.py
@@ -2,28 +2,30 @@ from manim import *
 import pygments.styles as code_styles
 from manim_speech import VoiceoverScene
 
-# from manim_speech.interfaces.azure import AzureSpeechSynthesizer
+from manim_speech.interfaces.azure import AzureSpeechSynthesizer
+
 # from manim_speech.interfaces.pyttsx3 import PyTTSX3SpeechSynthesizer
 # from manim_speech.interfaces.gtts import GTTSSpeechSynthesizer
 from manim_speech.interfaces.recording_mapper import RecordingMapper
 
 # import pyttsx3
 
-code_style = code_styles.get_style_by_name("monokai")
+code_style = code_styles.get_style_by_name("one-dark")
 
 
 class VoiceoverDemo(VoiceoverScene):
     def construct(self):
         # Initialize speech synthesis using Azure's TTS API
-        # self.set_speech_synthesizer(
-        #     AzureSpeechSynthesizer(
-        #         voice="en-US-AriaNeural", style="newscast-casual", #global_speed=1.15
-        #     )
-        # )
-        dirname = os.path.dirname(os.path.abspath(__file__))
         self.set_speech_synthesizer(
-            RecordingMapper(dirname + "/voiceover_demo_recording.mp3")
+            AzureSpeechSynthesizer(
+                voice="en-US-AriaNeural",
+                style="newscast-casual",  # global_speed=1.15
+            )
         )
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        # self.set_speech_synthesizer(
+        #     RecordingMapper(dirname + "/voiceover_demo_recording.mp3")
+        # )
 
         # self.set_speech_synthesizer(PyTTSX3SpeechSynthesizer(pyttsx3.init(), global_speed=1.15))
         # self.set_speech_synthesizer(GTTSSpeechSynthesizer())
@@ -80,14 +82,14 @@ self.play(Write(demo_code), run_time=tracker.duration)''',
         circle = Circle()
         square = Square().shift(2 * RIGHT)
 
-        with self.voiceover(text="This circle is drawn as I speak."):
-            self.play(Create(circle))
+        with self.voiceover(text="This circle is drawn as I speak.") as tracker:
+            self.play(Create(circle), run_time=tracker.duration)
 
-        with self.voiceover(text="Let's shift it to the left 2 units."):
-            self.play(circle.animate.shift(2 * LEFT))
+        with self.voiceover(text="Let's shift it to the left 2 units.") as tracker:
+            self.play(circle.animate.shift(2 * LEFT), run_time=tracker.duration)
 
-        with self.voiceover(text="Now, let's transform it into a square."):
-            self.play(Transform(circle, square))
+        with self.voiceover(text="Now, let's transform it into a square.") as tracker:
+            self.play(Transform(circle, square), run_time=tracker.duration)
 
         with self.voiceover(text="I would go on, but you get the idea."):
             self.play(FadeOut(circle))
@@ -95,18 +97,20 @@ self.play(Write(demo_code), run_time=tracker.duration)''',
         demo_code2 = Code(
             code="""class VoiceoverDemo(VoiceoverScene):
     def construct(self):
-        self.init_voiceover(AzureTTS(
-            voice="en-US-AriaNeural",
-            style="newscast-casual",
-            global_speed=1.15
-        ))
+        self.set_speech_synthesizer(
+            AzureSpeechSynthesizer(
+                voice="en-US-AriaNeural",
+                style="newscast-casual",
+                global_speed=1.15
+            )
+        )
         circle = Circle()
 
         with self.voiceover(text="This circle is drawn as I speak."):
             self.play(Create(circle))
 
-        with self.voiceover(text="Let's shift it to the left 2 units."):
-            self.play(circle.animate.shift(2 * LEFT))""",
+        with self.voiceover(text="Let's shift it to the left 2 units.") as tracker:
+            self.play(circle.animate.shift(2 * LEFT), run_time=tracker.duration)""",
             insert_line_no=False,
             style=code_style,
             background="window",
@@ -118,31 +122,31 @@ self.play(Write(demo_code), run_time=tracker.duration)''',
             self.play(FadeIn(demo_code2.background_mobject))
 
         with self.voiceover(
-            text="""First, we create a scene using
-            the Voiceover Scene class from the plugin."""
+            text="First, we create a scene using the Voiceover Scene class from the plugin."
         ):
             self.play(FadeIn(demo_code2.code[:2]))
 
         with self.voiceover(
-            text="""Then, we initialize the voiceover by giving it
-            the appropriate speech synthesizer. In this example, we use
-            Azure Text-to-speech."""
+            text="Then, we initialize the voiceover by setting the appropriate speech synthesizer."
         ):
             self.play(FadeIn(demo_code2.code[2]))
 
-        with self.voiceover(
-            text="""We use the English speaking neural voice called Aria."""
-        ):
+        with self.voiceover(text="In this example, we use Azure Text-to-speech."):
             self.play(FadeIn(demo_code2.code[3]))
 
-        with self.voiceover(text="""We use the style called "newscast casual"."""):
+        with self.voiceover(
+            text="We use the English speaking neural voice called Aria."
+        ):
             self.play(FadeIn(demo_code2.code[4]))
+
+        with self.voiceover(text='We use the style called "newscast casual".'):
+            self.play(FadeIn(demo_code2.code[5]))
 
         with self.voiceover(
             text="""Finally, we give an option to speed up the voiceover
             playback fifteen percent, because the default is a bit too slow."""
         ):
-            self.play(FadeIn(demo_code2.code[5:7]))
+            self.play(FadeIn(demo_code2.code[6:9]))
 
         with self.voiceover(
             text="""With the configuration out of the way, it is time to animate."""
@@ -150,20 +154,20 @@ self.play(Write(demo_code), run_time=tracker.duration)''',
             pass
 
         with self.voiceover(text="""Let's initialize the circle object."""):
-            self.play(FadeIn(demo_code2.code[7:8]))
+            self.play(FadeIn(demo_code2.code[9:11]))
 
         with self.voiceover(
             text="""Then, we need to tell the scene to start narrating,
             by calling the function "self-dot-voiceover"."""
         ):
-            self.play(FadeIn(demo_code2.code[9]))
+            self.play(FadeIn(demo_code2.code[11]))
 
         with self.voiceover(
             text="""By wrapping our animation inside a "with-statement",
             we ensure that once it finishes playing, it will also wait for
             the voiceover playback to finish."""
         ):
-            self.play(FadeIn(demo_code2.code[10]))
+            self.play(FadeIn(demo_code2.code[12]))
 
         with self.voiceover(
             text="""This is extremely convenient, and let's you chain
@@ -172,19 +176,110 @@ self.play(Write(demo_code), run_time=tracker.duration)''',
             pass
 
         with self.voiceover(
-            text="""We just need to repeat the same pattern with self-dot-voiceover and with-statements."""
+            text="""We just need to repeat the same pattern with self-dot-voiceover and with-statements. Here is something cool."""
         ):
-            self.play(FadeIn(demo_code2.code[11:]))
-
-        self.wait()
+            self.play(FadeIn(demo_code2.code[14]))
 
         with self.voiceover(
-            text="""You can use this Manim plugin to generate
-            voiceovers for your own projects."""
+            text="""We can retrieve the duration of the generated voiceover programmatically, and then use it to define for how long an animation should play."""
         ):
-            self.play(FadeOut(demo_code2))
+            self.play(FadeIn(demo_code2.code[15]))
 
-        with self.voiceover(text="Check out the GitHub repo shown on your screen."):
-            self.play(FadeIn(Tex("https://github.com/MathBlocks/manim-speech")))
+        demo_code3 = Code(
+            code="""class VoiceoverDemo(VoiceoverScene):
+    def construct(self):
+        self.set_speech_synthesizer(
+            AzureSpeechSynthesizer(
+                voice="en-US-AriaNeural",
+                style="newscast-casual",
+                global_speed=1.15
+            )
+        )
+        # self.set_speech_synthesizer(
+        #     RecordingMapper("my_voice_recording.mp3")
+        # )
+        """,
+            insert_line_no=False,
+            style=code_style,
+            background="window",
+            font="Consolas",
+            language="python",
+        ).scale(0.85)
+
+        demo_code4 = Code(
+            code="""class VoiceoverDemo(VoiceoverScene):
+    def construct(self):
+        # self.set_speech_synthesizer(
+        #     AzureSpeechSynthesizer(
+        #         voice="en-US-AriaNeural",
+        #         style="newscast-casual",
+        #         global_speed=1.15
+        #     )
+        # )
+        # self.set_speech_synthesizer(
+        #     RecordingMapper("my_voice_recording.mp3")
+        # )
+        """,
+            insert_line_no=False,
+            style=code_style,
+            background="window",
+            font="Consolas",
+            language="python",
+        ).scale(0.85).align_to(demo_code3, LEFT)
+
+        demo_code5 = Code(
+            code="""class VoiceoverDemo(VoiceoverScene):
+    def construct(self):
+        # self.set_speech_synthesizer(
+        #     AzureSpeechSynthesizer(
+        #         voice="en-US-AriaNeural",
+        #         style="newscast-casual",
+        #         global_speed=1.15
+        #     )
+        # )
+        self.set_speech_synthesizer(
+            RecordingMapper("my_voice_recording.mp3")
+        )
+        """,
+            insert_line_no=False,
+            style=code_style,
+            background="window",
+            font="Consolas",
+            language="python",
+        ).scale(0.85).align_to(demo_code3, LEFT)
+
+        with self.voiceover(text="And that's not even the best part! You can switch the AI generated voice with an actual recording of your voice very easily."):
+            self.play(FadeOut(demo_code2))
+            self.wait()
+            text1 = Tex("AI voice")
+            arrow = Tex(r"$\rightarrow$")
+            text2 = Tex("Voice recording")
+            VGroup(text1, arrow, text2).arrange(RIGHT)
+            self.play(Write(text1))
+            self.play(Write(arrow))
+            self.wait()
+            self.play(Write(text2))
+            self.wait()
+            self.play(FadeOut(text1, text2, arrow))
+
+        with self.voiceover(text="To do that, you record an MP3 of the final text of your video with 2 second pauses."):
+            self.play(FadeIn(demo_code3))
+
+        with self.voiceover(
+            text="""Then you simply replace the AI speech synthesizer with a class called recording mapper, giving your MP3 file as input.
+            Manim-speech automatically splits your audio and replaces the AI generated voice with your real recording, wherever it is needed."""
+        ):
+            self.play(FadeOut(demo_code3.code), FadeIn(demo_code4.code))
+            self.play(FadeOut(demo_code4.code), FadeIn(demo_code5.code))
+
+        self.wait(2)
+
+        with self.voiceover(
+            text="""Manim-speech makes it much easier to do voiceovers for Manim projects."""
+        ):
+            self.play(FadeOut(demo_code5.code, demo_code3.background_mobject))
+
+        with self.voiceover(text="Visit the GitHub repo shown on your screen to start using it in your project."):
+            self.play(FadeIn(Tex(r"\texttt{https://github.com/MathBlocks/manim-speech}")))
 
         self.wait(5)
